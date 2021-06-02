401e3773c6e787cfe6af648fcda101349d5fa88b
NKS <KishorNagare@Outlook.com>
2021-06-02 23:05:30 +0100

    swagger and othre config

---
+++ NKS.PaymentGateway.API/Configuration/Dependencies.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+namespace NKS.PaymentGateway.API.Configuration
+{
+    public static class Dependencies
+    {
+        public static IServiceCollection AddAPIConfiguration(this IServiceCollection services,IConfiguration config)
+        {
+            
+            var swaggerConfig = config.GetSection("SwaggerConfiguration").Get<Swagger>();
+            
+            services
+                .AddMvcCore()
+                .AddMvcOptions(options =>
+                {
+                    //options.Filters.Add<UnitOfWork>(1);
+                    //options.Filters.Add<LogRequestTimeFilterAttribute>();
+                });
+
+            services.AddSwaggerGen(options =>
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlCommentsPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                options.IncludeXmlComments(xmlCommentsPath);
+                options.SwaggerDoc($"v{swaggerConfig.Version}", new OpenApiInfo
+                {
+                    Title = swaggerConfig.Title,
+                    Version = $"v{swaggerConfig.Version}",
+                    Description = swaggerConfig.Description,
+
+                });
+                options.EnableAnnotations();
+            });
+            return services;
+        }
+    }
+}
--- NKS.PaymentGateway.API/Startup.cs
+++ NKS.PaymentGateway.API/Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NKS.PaymentGateway.API.Configuration;
+using Serilog;
 
 namespace NKS.PaymentGateway.API
 {
@@ -26,12 +28,10 @@ namespace NKS.PaymentGateway.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NKS.PaymentGateway.API", Version = "v1" });
-            });
+            Log.Information("Configuring services.");
+          //  services.Configure<Swagger>(Configuration.GetSection("SwaggerConfiguration"));
+          services.AddControllers(); 
+          services.AddAPIConfiguration(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
