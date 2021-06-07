5c7e2be5843af20d9ff9d1079cc06b985cd66ceb
NKS <KishorNagare@Outlook.com>
2021-06-07 08:25:06 +0100

    auth and DynamoDBClient

---
+++ NKS.PaymentGateway.API/Interfaces/IUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace NKS.Payments.API.Interfaces
+{
+    using System;
+
+    public interface IUserService
+    {
+        bool ValidateCredentials(String username, String password);
+    }
+}
---
+++ NKS.PaymentGateway.API/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿namespace NKS.Payments.API.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Interfaces;
+
+    public class UserService: IUserService
+    {
+        public bool ValidateCredentials(string username, string password)
+        {
+            return username.Equals("me") && password.Equals("Pa$$WoRd");
+        }
+    }
+}
